2cb9753559718095741bbd4ba9000b597525e9c8
Tim Schraepen <sch3lp@gmail.com>
2016-07-13 17:14:55 +0200

    initial commit

---
+++ automaton/solution.js
@@ -0,0 +1,32 @@
+function Automaton() {
+   this.states = [];
+}
+
+Automaton.prototype.readCommands = function(commands) {
+  // Return true if we end in our accept state, false otherwise.
+}
+
+let States = (function() {
+	
+	let StateProto = function(){
+		return {
+			transition: function(letter) {
+				return n;
+			}
+		}
+	}
+
+	function BeginStatePrototype() {
+
+	}
+
+	return {
+		Q1: new BeginStatePrototype(),
+		Q2: new AcceptStatePrototype(),
+		Q3: new WrongStatePrototype()
+	}
+})();
+
+var myAutomaton = new Automaton();
+
+// Do anything necessary to set up your automaton's states, q1, q2, and q3.
