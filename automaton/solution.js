f4962f55c9a4c2d434c4b9fcb00713b97c8a8551
Tim Schraepen <sch3lp@gmail.com>
2016-07-15 18:52:18 +0200

    working tests

--- automaton/solution.js
+++ automaton/solution.js
@@ -1,36 +1,49 @@
-module.exports = () => {
+/* jshint undef: true, unused: false, esnext: true, strict:false */
+/* globals module */
+
+module.exports = (() => {
 
 function Automaton() {
    this.states = [];
 }
 
+Automaton.prototype.isValidDialect = function(commands) {
+	return commands.filter((command) => {
+		return ['0','1'].includes(command);
+	}).length == commands.length;
+};
+
+Automaton.prototype.transition = function(commands) {
+	let currentState = new States.Q1();
+	commands.forEach((command) => {
+		let CurrentState = currentState.transition(command);
+		currentState = new CurrentState();
+	});
+	return currentState;
+};
+
 Automaton.prototype.readCommands = function(commands) {
-  // Return true if we end in our accept state, false otherwise.
-}
+	return this.isValidDialect(commands) ? this.transition(commands).isAcceptState() : false;
+};
+
 
 let States = (function() {
-	
-	let StateProto = function(){
-		return {
-			transition: function(letter) {
-				return n;
-			}
-		}
-	}
+	function Q1() {}
+	function Q2() {}
+	function Q3() {}
 
-	function BeginStatePrototype() {
+	Q1.prototype.transition = (command) => command === '1' ? Q2 : Q1;
+	Q2.prototype.transition = (command) => command === '1' ? Q2 : Q3;
+	Q3.prototype.transition = (_) => Q2;
+	Q1.prototype.isAcceptState = () => false;
+	Q2.prototype.isAcceptState = () => true;
+	Q3.prototype.isAcceptState = () => false;
 
-	}
-
-	return {
-		Q1: new BeginStatePrototype(),
-		Q2: new AcceptStatePrototype(),
-		Q3: new WrongStatePrototype()
-	}
+	return { Q1, Q2, Q3 };
 })();
 
-var myAutomaton = new Automaton();
-
+return { Automaton, States };
 // Do anything necessary to set up your automaton's states, q1, q2, and q3.
-};
+
+})();
 
--- automaton/test.js
+++ automaton/test.js
@@ -1,30 +1,84 @@
 let expect = require('chai').expect;
-let solution = require('./solution');
+let {Automaton, States} = require('./solution');
 
+var myAutomaton = new Automaton();
 
 describe('Q1', () => {
 	it('should transition correctly', () => {
-		expect(new Q2()).to.equal(new Q1().transition(1));
-		expect(new Q1()).to.equal(new Q1().transition(0));
+		let state = new States.Q1();
+		expect(state.transition('1')).to.equal(States.Q2);
+		expect(state.transition('0')).to.equal(States.Q1);
 	});
 });
 
 describe('Q2', () => {
 	it('should transition correctly', () => {
-		expect(new Q2()).to.equal(new Q2().transition(1));
-		expect(new Q3()).to.equal(new Q2().transition(0));
+		let state = new States.Q2();
+		expect(state.transition('1')).to.equal(States.Q2);
+		expect(state.transition('0')).to.equal(States.Q3);
 	});
 });
 
 describe('Q3', () => {
 	it('should transition correctly', () => {
-		expect(new Q2()).to.equal(new Q3().transition(1));
-		expect(new Q2()).to.equal(new Q3().transition(0));
+		let state = new States.Q3();
+		expect(state.transition('1')).to.equal(States.Q2);
+		expect(state.transition('0')).to.equal(States.Q2);
 	});
 });
 
-describe('myAutomaton', () => {
-	it('should transition correctly', () => {
-		
+describe('isValidDialect', () => {
+	it('non strings are not valid', () => {
+		expect(myAutomaton.isValidDialect([1])).to.be.false;
+		expect(myAutomaton.isValidDialect(['1',1])).to.be.false;
+	});
+
+	it('unknown strings are not valid', () => {
+		expect(myAutomaton.isValidDialect(['2'])).to.be.false;
+		expect(myAutomaton.isValidDialect(['1','2'])).to.be.false;
+	});
+
+	it('only strings 0 and 1 are valid', () => {
+		expect(myAutomaton.isValidDialect(['0'])).to.be.true;
+		expect(myAutomaton.isValidDialect(['1'])).to.be.true;
+		expect(myAutomaton.isValidDialect(['0', '1'])).to.be.true;
+		expect(myAutomaton.isValidDialect(['1', '0', '0', '1'])).to.be.true;
 	});
 });
+
+describe('transition', () => {
+	it('can result in Q1', () => {
+		expect(myAutomaton.transition(['0'])).to.be.an.instanceof(States.Q1);
+		expect(myAutomaton.transition(['0','0','0'])).to.be.an.instanceof(States.Q1);
+	});
+
+	it('can result in Q2', () => {
+		expect(myAutomaton.transition(['1'])).to.be.an.instanceof(States.Q2);
+		expect(myAutomaton.transition(['1','1'])).to.be.an.instanceof(States.Q2);
+		expect(myAutomaton.transition(['1','0', '0', '1'])).to.be.an.instanceof(States.Q2);
+	});
+
+	it('can result in Q3', () => {
+		expect(myAutomaton.transition(['1','0'])).to.be.an.instanceof(States.Q3);
+		expect(myAutomaton.transition(['1','0', '0', '1', '0'])).to.be.an.instanceof(States.Q3);
+	});
+});
+
+describe('readCommands', () => {
+	it('can result in Q1', () => {
+		expect(myAutomaton.readCommands(['0'])).to.be.false;
+		expect(myAutomaton.readCommands(['0','0','0'])).is.false;
+	});
+
+	it('can result in Q2', () => {
+		expect(myAutomaton.readCommands(['1'])).to.be.true;
+		expect(myAutomaton.readCommands(['1','1'])).to.be.true;
+		expect(myAutomaton.readCommands(['1','0', '0', '1'])).to.be.true;
+	});
+
+	it('can result in Q3', () => {
+		expect(myAutomaton.readCommands(['1','0'])).to.be.false;
+		expect(myAutomaton.readCommands(['1','0', '0', '1', '0'])).to.be.false;
+	});
+});
+
